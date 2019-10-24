b0be174fc5089f630770feedcf6cd25a53063723
Bray <bsst@M-C02ZC53WLVDQ.nordstrom.net>
2019-10-24 14:28:24 -0700

    renamed file

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="junipercrypt-zachbray",
-    version="0.0.1",
+    name="junipercrypt",
+    version="0.0.2",
     author="Zach Bray",
     author_email="zachbray123@gmail.com",
     description="Encrypt/decrypts juniper $9$ secrets",
